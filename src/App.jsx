8ae6da8f38ef6d075fa874f60ab25b82661efe33
mahendharjillala <jillalamahendhar@gmail.com>
2025-09-13 08:46:33 +0530

    jenkins demo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <div>
+      <h1> CiCd Jenkins Demo</h1>
+      <p> 
+        Jenkins sorce code replaced with users git repo and set branch /main 
+      </p>
+    </div>
+  )
+}
+
+export default App
