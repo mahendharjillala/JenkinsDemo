6fbded4a3585956f1c0016e2c3f25da6e1283314
mahendharjillala <jillalamahendhar@gmail.com>
2025-09-15 10:36:51 +0530

    Update App.jsx with Jenkins build details

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-
 import './App.css'
 
 function App() {
-  
   return (
     <div>
-      <h1> CiCd Jenkins Demo</h1>
-      <p> 
-        Jenkins sorce code replaced with users git repo and set branch /main 
-      </p>
+      <div>
+        <h1>CICD JENKINS DEMO</h1>
+        <p>
+          Jenkins source code uses git repo and set branch <b>/main</b>.  
+          Poll SCM triggers option <code>* * * * *</code>.  
+          Build step → <b>Windows batch command</b>:  
+          <u>call npm install</u>  
+          <u>call npm run dev</u>  
+          rmdir "C:\......tomcat9\webapps\jenkinsfrontdemo"  
+          mkdir "C:\......tomcat9\webapps\jenkinsfrontdemo"  
+          xcopy path
+        </p>
+      </div>
+
+      <div>
+        <p>
+          Build step → <b>Windows batch command</b>:  
+          stop tomcat 9  
+          start tomcat 9
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
